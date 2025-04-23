1b88a8ab15a8e2c2959eb4e418b1bbc2c96a2378
PedroDeMiranda <pedromirandaneto.2006@gmail.com>
2025-04-22 22:22:21 -0300

    Adiciona .gitignore e remove arquivos temporáos do versionamento

---
+++ BackEnd/Back-End/Infra/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Back_End.Model;
+
+namespace Back_End.Infra.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
