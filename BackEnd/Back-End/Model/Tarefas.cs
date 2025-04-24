a5d1c93089dd7e8e9a2f1b3717c19596c8a8b47b
tiagoGrigull <tagrigull@gmail.com>
2025-04-24 20:01:06 -0300

    atualização

--- BackEnd/Back-End/Model/Tarefas.cs
+++ BackEnd/Back-End/Model/Tarefas.cs
@@ -1,6 +1,21 @@
-﻿namespace Back_End.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Back_End.Model
 {
     public class Tarefas
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+
     }
 }
--- BackEnd/Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
+++ BackEnd/Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84184187c67b8c04441dc8702940a12bc222c75f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46006d01b8e72646151c5aaf1faf6ebecee1cce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
