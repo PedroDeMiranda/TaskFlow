e9300ea205aa546911063903d08392d5261a2395
tiagoGrigull <tagrigull@gmail.com>
2025-04-28 20:14:26 -0300

    TarefaRepository feito

--- BackEnd/Back-End/Controllers/TarefasController.cs
+++ BackEnd/Back-End/Controllers/TarefasController.cs
@@ -1,12 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Back_End.Model;
+using Back_End.Model.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Back_End.Controllers
 {
+
+    [ApiController]
     public class TarefasController : Controller
     {
-        public IActionResult Index()
+        private readonly ITarefaRepository _tarefaRepository;
+
+        public TarefasController(ITarefaRepository repository)
         {
-            return View();
+            _tarefaRepository = repository;
+        }
+
+        [HttpGet("api/tarefas")]
+        public IActionResult GetTarefas()
+        {
+            try
+            {
+                var tarefas = _tarefaRepository.Listar();
+                return Ok(tarefas);
+            }
+            catch (Exception e)
+            {
+                return Unauthorized(e);
+            }
+        }
+
+        [HttpPost("api/tarefas")]
+        public IActionResult Salvar(Tarefa tarefas)
+        {
+            try
+            {
+
+                var lRusltoSalvarusuario = _tarefaRepository.Salvar(tarefas).Result;
+                return Ok(lRusltoSalvarusuario);
+
+            }
+            catch (Exception error)
+            {
+                return ValidationProblem(new ValidationProblemDetails() { Detail = error.Message });
+            }
+        }
+
+        [HttpDelete("api/tarefas/{id}")]
+        public IActionResult excluir(int id)
+        {
+            try
+            {
+                var retorno = _tarefaRepository.Excluir(id).Result;
+                return Ok(retorno);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+
+        }
+
+        [HttpGet("api/tarefas/{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                var retorno = _tarefaRepository.BuscarporId(id).Result;
+                return Ok(retorno);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+
         }
     }
 }
--- BackEnd/Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
+++ BackEnd/Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d1c93089dd7e8e9a2f1b3717c19596c8a8b47b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a8dad799b8942a808e2f39eff7a0910259a29e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
